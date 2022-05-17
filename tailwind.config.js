3bdf545ce8298f82d146536105c0860b9a6476db
chanyoung <kipa1003@naver.com>
2022-05-18 00:13:10 +0900

    tailwind 색상 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 module.exports = {
 	content: ['./src/**/*.tsx', './public/index.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primaryOrange: {
+					100: '#FFEEDB',
+					200: '#FF8A00',
+					300: '#E47B00',
+				},
+				primaryBlack: {
+					100: '#E7E7E7',
+					200: '#C1C1C1',
+					300: '#A6A6A6',
+					400: '#8B8B8B',
+					500: '#000000',
+				},
+				primaryWhite: '#FFFFFF',
+				buttonBlack: {
+					100: '#F4F4F4',
+					200: '#FF1523',
+				},
+				buttonOrange: {
+					100: '#FFEEDB',
+					200: '#FF1523',
+				},
+				buttonRed: {
+					100: '#FFE8E8',
+					200: '#FF6B6B',
+				},
+				borderOrange: '#FF8A00',
+				statesRed: '#FF1523',
+				statesOrange: '#FF8A00',
+			},
+		},
 	},
 	plugins: [],
 };
