b8eb48b28dc45e245c2eb77c3a2ef3f7f41271fc
정예원 <yewonn.1j@gmail.com>
2022-05-25 18:00:11 +0900

    image & folder structure

---
+++ src/pages/Login/LoginContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginView from './LoginView';
+
+function LoginContainer() {
+	return <LoginView />;
+}
+
+export default LoginContainer;
---
+++ src/pages/Login/LoginView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginView() {
+	return (
+		<div className="pc:w-[1200px] my-0 mx-auto">
+			<div className="bg-primaryOrange-200 rounded-[16px] p-[55px] w-[480px] flex flex-col items-center ">
+				<img src="image/banner/image1773.png" alt="banner" />
+			</div>
+		</div>
+	);
+}
+
+export default LoginView;
