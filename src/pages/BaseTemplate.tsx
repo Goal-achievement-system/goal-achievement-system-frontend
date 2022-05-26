919ce0f1085bf48545f4d915c9f86604a5c0fbdf
ahn0min <ahn0min95@gmail.com>
2022-05-27 00:06:28 +0900

    feat: click event에 의한 Modal 기능 구현

--- src/pages/BaseTemplate.tsx
+++ src/pages/BaseTemplate.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useRef, useState } from 'react';
 import Header from 'components/Header/Header';
 
 interface TemplateProps {
@@ -7,9 +8,37 @@ interface TemplateProps {
 }
 
 export default function BaseTemplate({ children, Modal }: TemplateProps) {
-	<div className="w-full h-full main">
-		<Header />
-		<section>{children}</section>
-		{/* <Modal /> */}
-	</div>;
+	const modalRef = useRef<HTMLDivElement>(null);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const handleClick = (e: React.BaseSyntheticEvent | MouseEvent) => {
+		if (modalRef.current !== null && !modalRef.current.contains(e.target)) {
+			setIsOpen(false);
+			document.body.style.cssText = '';
+		}
+	};
+	const modalSection = (
+		<div className="absolute top-0 left-0 w-full h-full z-1000" onClick={handleClick} aria-hidden>
+			<div className="absolute w-full h-full z-999 bg-primaryBlack-500 bg-opacity-20" />
+			<div className="relative top-1/2 left-1/2" ref={modalRef}>
+				{Modal}
+			</div>
+		</div>
+	);
+
+	return (
+		<div className="fixed w-full h-full main">
+			<Header />
+			<section
+				onClick={() => {
+					setIsOpen(true);
+					document.body.style.cssText = 'overflow : hidden';
+				}}
+				aria-hidden
+			>
+				{children}
+			</section>
+			{Modal && isOpen && modalSection}
+		</div>
+	);
 }
