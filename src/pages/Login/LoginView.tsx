22502f6a37e766328fdbc07b96c3b97a3edbeef0
정예원 <yewonn.1j@gmail.com>
2022-05-26 13:28:39 +0900

    banner

--- src/pages/Login/LoginView.tsx
+++ src/pages/Login/LoginView.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 function LoginView() {
 	return (
-		<div className="pc:w-[1200px] my-0 mx-auto">
-			<div className="bg-primaryOrange-200 rounded-[16px] p-[55px] w-[480px] flex flex-col items-center ">
-				<img src="image/banner/image1773.png" alt="banner" />
+		<div className="pc:w-[1200px] my-0 mx-auto flex justify-between">
+			<div className="bg-primaryOrange-200 rounded-[16px] p-12 w-1/3 flex flex-col items-start ">
+				<img src="../image/banner/image1773.png" alt="banner" />
+				<img className="my-4" src="../image/logo/img--logo.png" alt="logo" />
+
+				<img className="w-4/5" src="../image/text/slogan.png" alt="slogan" />
 			</div>
 		</div>
 	);
