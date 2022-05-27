05d5391ff92aa5fbf9f1680e6fd3ee0f32cafb7b
ahn0min <ahn0min95@gmail.com>
2022-05-27 17:37:48 +0900

    component 작동 확인을 위한 Home.tsx 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
+import React from 'react';
+import GoalAddPopUp from 'components/Modal/GoalAddModal';
+import GoalPopUp from 'components/Modal/GoalModal';
+import { Goal } from 'types/goal';
+import BaseTemplate from './BaseTemplate';
+
 function Home() {
-	return null;
+	const goal: Goal = {
+		goalId: 1,
+		memberEmail: '',
+		category: '취미',
+		goalName: '목표인증 텍스트',
+		content: '목표 내용',
+		limitDate: new Date(2022, 5, 25),
+		money: 10000,
+		reward: 'high',
+		verificationResult: 'ongoing',
+	};
+	return (
+		<BaseTemplate Modal={<GoalPopUp goal={goal} />}>
+			<div>children</div>
+		</BaseTemplate>
+	);
 }
 
 export default Home;
