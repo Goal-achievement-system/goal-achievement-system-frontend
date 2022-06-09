0c98bc40290d1241a32fa3b9cb778b29f4dadf91
YeongMin Ahn <ahn0min95@gmail.com>
2022-06-10 04:07:21 +0900

    나의 목표페이지 알람 ui 작업 추가

--- src/pages/MyGoal/MyGoalView.tsx
+++ src/pages/MyGoal/MyGoalView.tsx
@@ -8,12 +8,15 @@ import SmallBox from 'components/Box/SmallBox';
 import Pagination from 'components/Pagination';
 
 interface Props {
-	goals: Goal[];
+	goals: Goal[] | null;
 	notificationList: Notification[];
 	currentPage: number;
 	setCurrentPage: Dispatch<SetStateAction<number>>;
 	goalFilter: VerificationResult;
 	setGoalFilter: Dispatch<SetStateAction<VerificationResult>>;
+	isSelected: string;
+	setIsSelected: Dispatch<SetStateAction<string>>;
+	maxPage: number;
 }
 
 export default function MyGoalView({
@@ -23,6 +26,9 @@ export default function MyGoalView({
 	setCurrentPage,
 	goalFilter,
 	setGoalFilter,
+	isSelected,
+	setIsSelected,
+	maxPage,
 }: Props) {
 	// 버튼에 해당하는 현재 state
 	const getFilterState = (key: string) => {
@@ -39,7 +45,7 @@ export default function MyGoalView({
 	};
 
 	return (
-		<div>
+		<div className="flex-1 overflow-auto">
 			<Main title="내목표">
 				<div
 					className="flex space-x-[8px]"
@@ -52,22 +58,35 @@ export default function MyGoalView({
 					}}
 					aria-hidden
 				>
-					<FilterButton label="전체" isSelected onClick={() => {}} />
-					<FilterButton label="등록 전" isSelected={false} onClick={() => {}} />
-					<FilterButton label="진행 중" isSelected={false} onClick={() => {}} />
-					<FilterButton label="성공" isSelected={false} onClick={() => {}} />
-					<FilterButton label="실패" isSelected={false} onClick={() => {}} />
-					<FilterButton label="보류" isSelected={false} onClick={() => {}} />
+					{['전체', '등록 전', '진행 중', '성공', '실패', '보류'].map((ele: string) => {
+						return (
+							<div key={ele}>
+								<FilterButton label={ele} isSelected={ele === isSelected} onClick={() => setIsSelected(ele)} />
+							</div>
+						);
+					})}
 				</div>
-				<div className="box-wrap pc:my-[30px]">{goals && goals.map((goal) => <SmallBox goal={goal} />)}</div>
+				<div className="box-wrap pc:my-[30px]">
+					{goals ? (
+						<ul className="grid grid-cols-3 pc:gap-[30px] gap-[16px]">
+							{goals.map((goal) => (
+								<li key={goal.goalId}>
+									<SmallBox goal={goal} />
+								</li>
+							))}
+						</ul>
+					) : (
+						<div className="text-center">&#34;{isSelected}&#34; 인 상태의 목표가 없습니다.</div>
+					)}
+				</div>
 				<div className="flex content-center ">
-					<Pagination numOfPages={3} setCurPage={setCurrentPage} curPage={currentPage} numOfPageBtn={3} />
+					<Pagination curPage={currentPage} setCurPage={setCurrentPage} numOfPages={maxPage} numOfPageBtn={1} />
 				</div>
 			</Main>
 			<div className="pc:mt-[30px]">
 				<Main title="알림">
-					<div className="notifiaction-wrap">
-						{notificationList.map(({ category, sendingTime, message, link }: Notification) => {
+					<div className="flex flex-col pc:space-y-[16px]">
+						{/* {notificationList.map(({ category, sendingTime, message, link }: Notification) => {
 							return (
 								<div>
 									<div>{category}</div>
@@ -75,7 +94,29 @@ export default function MyGoalView({
 									<div>{sendingTime}</div>
 								</div>
 							);
-						})}
+						})} */}
+						{[
+							{ category: 'category', sendingTime: '2022-02-02', message: '메세지입니다.', link: '/' },
+							{ category: 'category', sendingTime: '2022-02-02', message: '첫 목표등록이 완료되었어요.', link: '/' },
+							{
+								category: 'categorycategorycategorycategory',
+								sendingTime: '2022-02-02',
+								message:
+									'메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다.메세지입니다',
+								link: '/',
+							},
+							{ category: 'category', sendingTime: '2022-02-02', message: '메세지입니다.', link: '/' },
+							{ category: 'category', sendingTime: '2022-02-02', message: '메세지입니다.', link: '/' },
+							{ category: 'category', sendingTime: '2022-02-02', message: '메세지입니다.', link: '/' },
+						].map(({ category, sendingTime, message, link }) => (
+							<div className="border-2 bg-alarmGray  border-borderGray pc:px-[24px] pc:pt-[24px] pc:pb-[20px] rounded-[16px] pc:h-[108px] flex flex-col justify-between">
+								<div className="flex justify-between">
+									<div className="text-primaryOrange-200 w-[100px] line-clamp-1">{category}</div>
+									<div className="">{sendingTime}</div>
+								</div>
+								<div className="line-clamp-1">{message}</div>
+							</div>
+						))}
 					</div>
 				</Main>
 			</div>
