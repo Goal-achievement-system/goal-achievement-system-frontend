596f6b19cdc1755d7d60f0b981495bc03ac2a533
정예원 <yewonn.1j@gmail.com>
2022-06-09 15:32:50 +0900

    목표등록 api 추가

--- src/api/goalAPI.ts
+++ src/api/goalAPI.ts
@@ -1,8 +1,13 @@
 import { LoadGoalParam } from 'store/sagas/goalSaga';
+import { GoalFormState } from 'pages/Goal/GoalRegister/RegisterType';
 import client from './client';
 
 export const loadGoaliLst = (params: LoadGoalParam) => {
 	return client.get(`goals/${params.category}/list/${params.status}/${params.page}`);
+};
+
+export const registerGoal = (params: GoalFormState) => {
+	return client.post(`goals`, { ...params });
 };
 
 export default loadGoaliLst;
