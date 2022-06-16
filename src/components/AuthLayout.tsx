7d3de9fe462b46ecbe63e77c6298652aed0192d9
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-06-17 02:01:19 +0900

    Update AuthLayout.tsx

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 function AuthLayout({ children }: Props) {
 	return (
-		<div className="my-0 mx-auto h-[300px] pc:w-[1200px] pc:h-[762px]  pc:mt-[20px] pc:flex pc:justify-between">
+		<div className=" mx-auto overflow-hidden h-full pc:w-[1200px] pc:h-[762px]  pc:mt-[20px] pc:flex pc:justify-between">
 			<div className="bg-primaryOrange-200  invisible  w-0 h-0  flex flex-col items-start pc:rounded-[16px] pc:h-full pc:p-[55px] pc:visible pc:w-[481px] ">
 				<img src="../image/banner/image1773.png" alt="banner" />
 				<img className="my-[57px] w-[157px] h-[34px]" src="../image/logo/img--logo.png" alt="logo" />
