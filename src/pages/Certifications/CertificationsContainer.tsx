bb9495f385c65ecde4fdb9d49b094caa78326931
정예원 <yewonn.1j@gmail.com>
2022-06-21 17:22:50 +0900

    카테고리별로 인증목표 불러오기&페이지네이션 maxPage 적용

--- src/pages/Certifications/CertificationsContainer.tsx
+++ src/pages/Certifications/CertificationsContainer.tsx
@@ -1,22 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import useGetActionState from 'hooks/useGetActionState';
 import { AppDispatch } from 'store';
 import { RootState } from 'store/slices';
 import certificationSlice from 'store/slices/certificationSlice';
 import memberSlice from 'store/slices/memberSlice';
+import goalSlice from 'store/slices/goalSlice';
 import CertificationsView from './CertificationsView';
 
 function CertificationsContainer() {
 	const dispatch: AppDispatch = useDispatch();
-	const { goalList } = useSelector((state: RootState) => state.certification);
-	const [loading, result, initResult] = useGetActionState(certificationSlice.actions.loadCertGoalList.type);
-
+	const { goalList, maxPage } = useSelector((state: RootState) => state.certification);
+	const [certLoading, certResult, certInitResult] = useGetActionState(certificationSlice.actions.loadCertGoalList.type);
+	const [categoriesLoading, categoriesResult, categoriesInitResult] = useGetActionState(
+		goalSlice.actions.loadCategories.type
+	);
+	const categories = useSelector((state: RootState) => state.goal.categories);
+	const [curCategory, setCurCategory] = useState<string>('all');
 	useEffect(() => {
-		dispatch(certificationSlice.actions.loadCertGoalList({ category: 'all', page: 1 }));
+		if (categoriesLoading) return;
+		dispatch(goalSlice.actions.loadCategories());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [dispatch]);
+	useEffect(() => {
+		if (categoriesResult?.isSuccess) {
+			// success
+		} else {
+			// fail
+		}
+		categoriesInitResult();
+	}, [categoriesResult, categoriesInitResult]);
+	useEffect(() => {
+		if (certLoading) return;
+		if (![...categories, 'all'].includes(curCategory)) return;
+		dispatch(certificationSlice.actions.loadCertGoalList({ category: curCategory, page: 1 }));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [dispatch, curCategory]);
 
-	return <CertificationsView isLoading={loading} goalList={goalList} />;
+	return (
+		<CertificationsView
+			isCategoryLoading={categoriesLoading}
+			categories={categories}
+			isCertLoading={certLoading}
+			goalList={goalList}
+			curCategory={curCategory}
+			setCurCategory={setCurCategory}
+			maxPage={maxPage}
+		/>
+	);
 }
 
 export default CertificationsContainer;
