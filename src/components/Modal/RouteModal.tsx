32a2b25a5b458269f47931adb04112c2f5f036de
ahn0min <ahn0min95@gmail.com>
2022-07-13 03:13:49 +0900

    알람 관련 UI 수정

--- src/components/Modal/RouteModal.tsx
+++ src/components/Modal/RouteModal.tsx
@@ -3,53 +3,17 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from 'store/slices';
 import handleLogout, { handleAdminLogout } from 'utils/handleLogout';
-import AlarmPopup from './AlarmModal';
+import Path from 'utils/path';
+import AlarmPopup from './PushNoticeModal';
 import UserProfilePopUp from './UserProfileModal';
 
 interface Props {
 	title: '로그인 관리' | '알림';
-	// isLogin: boolean;
-	alarmList?: Alarm[];
 	isOpen: boolean;
 	setIsOpen?: Dispatch<SetStateAction<boolean[]>>;
 }
 
-interface Alarm {
-	title: string;
-	content: string;
-	date: number;
-}
-
-const data = [
-	{
-		title: 'title',
-		content: '골키퍼를 소개할게요!',
-		date: 1,
-	},
-	{
-		title: 'title',
-		content:
-			"'골키퍼 디자인 하기' 목표등록이 완료되었어요. 지금 확인해보세요' '골키퍼 디자인 하기' 목표등록이 완료되었어요. 지금 확인해보세요'",
-		date: 2,
-	},
-	{
-		title: 'title',
-		content: '검토 요청완료! 빠르게 검토 후 알려드릴게요',
-		date: 3,
-	},
-	{
-		title: 'title',
-		content: '검토 요청완료! 빠르게 검토 후 알려드릴게요',
-		date: 4,
-	},
-	{
-		title: 'title',
-		content: '검토 요청완료! 빠르게 검토 후 알려드릴게요',
-		date: 5,
-	},
-];
-
-export default function RouteModal({ title, alarmList = data, isOpen, setIsOpen }: Props) {
+export default function RouteModal({ title, isOpen, setIsOpen }: Props) {
 	const { isAdmin } = useSelector((state: RootState) => state.admin);
 	const { memberinfo } = useSelector((state: RootState) => state.member);
 	// eslint-disable-next-line consistent-return
@@ -116,15 +80,20 @@ export default function RouteModal({ title, alarmList = data, isOpen, setIsOpen 
 	}
 
 	return (
-		<div className="absolute right-0 z-50">
+		<div className="absolute right-0 z-50 scroll">
 			<div
 				className={`w-[380px] ${
 					title === '알림' ? 'h-[391px]' : 'h-[286px]'
-				} p-6 flex flex-col rounded-2xl border-2 text-left bg-white ${isOpen ? '' : 'hidden'}`}
+				} p-6 rounded-2xl border-2 text-left bg-white ${isOpen ? '' : 'hidden'}`}
 			>
-				<div className="font-[600] absolute">{title}</div>
+				<div className="font-[600] flex justify-between">
+					<div>{title}</div>
+					<div className="text-primaryBlack-300">
+						<Link to={Path.pushNotice}>더보기</Link>
+					</div>
+				</div>
 				{title === '알림' ? (
-					<div className="pc:my-auto overflow-auto pc:max-h-[270px] flex flex-col justify-center pc:min-h-[270px]">
+					<div className="pc:my-auto overflow-auto pc:mt-[10px] pc:max-h-[290px] h-full justify-center pc:min-h-[290px]">
 						<AlarmPopup />
 					</div>
 				) : (
