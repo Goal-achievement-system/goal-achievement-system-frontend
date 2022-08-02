68c761968afbac3bdb4e4f2b0b5e87d49a04628e
chanyoung <kipa1003@naver.com>
2022-08-02 18:00:52 +0900

    fix : API 포트 변경

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,6 +3,6 @@ import axios, { AxiosRequestConfig } from 'axios';
 // 모든 요청에 설정을 하지 않기 위해 instance생성
 const client = axios.create();
 
-client.defaults.baseURL = 'https://www.robinjoon.xyz/api/';
+client.defaults.baseURL = 'https://www.robinjoon.xyz:4134/api/';
 
 export default client;
