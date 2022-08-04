753e69bfa460738ced202dffad1959cf166fd3fb
정예원 <yewonn.1j@gmail.com>
2022-08-04 14:35:11 +0900

    feat : 코드 스플리팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Path from 'utils/path';
-import Home from 'pages/home/HomeContainer';
-import Login from 'pages/Login/LoginContainer';
-import SignUp from 'pages/SignUp/SignUpContainer';
-import Certifications from 'pages/Certifications/CertificationsContainer';
-import Notice from 'pages/Announcements/AnnouncementsContainer';
-import NotFound from 'pages/NotFound';
 import Layout from 'components/Layout';
-import Setting from 'pages/Setting/SettingContainer';
-import AnnouncementsDetail from 'pages/Announcements/AnnouncementsDetail';
 
-/*
-routes
-
-home : "/"
-login : "/login"
-sign-up : "/sign-up"
-certificacations:"/certifications"
-*/
+const AnnouncementsDetail = lazy(() => import('pages/Announcements/AnnouncementsDetail'));
+const Login = lazy(() => import('pages/Login/LoginContainer'));
+const SignUp = lazy(() => import('pages/SignUp/SignUpContainer'));
+const Setting = lazy(() => import('pages/Setting/SettingContainer'));
+const NotFound = lazy(() => import('pages/NotFound'));
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
-				<Routes>
-					<Route path={Path.announcementsDetail} element={<AnnouncementsDetail />} />
-					<Route path={Path.login} element={<Login />} />
-					<Route path={Path.signUp} element={<SignUp />} />
-					<Route path={Path.setting} element={<Setting />} />
-					<Route path="/*" element={<Layout />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+				<Suspense fallback={<div>Loading..</div>}>
+					<Routes>
+						<Route path="/*" element={<Layout />} />
+						<Route path={Path.announcementsDetail} element={<AnnouncementsDetail />} />
+						<Route path={Path.login} element={<Login />} />
+						<Route path={Path.signUp} element={<SignUp />} />
+						<Route path={Path.setting} element={<Setting />} />
+
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Suspense>
 			</Router>
 		</div>
 	);
 }
 export default App;
+
+/*
+
+*/
