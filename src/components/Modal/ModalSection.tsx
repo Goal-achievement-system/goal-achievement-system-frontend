e935989bce2875f4a849def00a509099b1231b2d
YeongMin Ahn <ahn0min95@gmail.com>
2022-09-27 23:00:50 +0900

    fix: modal창 바깥쪽을 클릭시 close 되지 않는 문제 해결

--- src/components/Modal/ModalSection.tsx
+++ src/components/Modal/ModalSection.tsx
@@ -7,6 +7,7 @@ import { Router, useLocation, useNavigate, useSearchParams } from 'react-router-
 import Path from 'utils/path';
 
 export default function ModalSection() {
+	const bodyRef = useRef<HTMLDivElement>(null);
 	const modalRef = useRef<HTMLDivElement>(null);
 	const { openList, isOpenModal } = useSelector((state: RootState) => state.modal);
 	const dispatch = useDispatch();
@@ -28,20 +29,24 @@ export default function ModalSection() {
 	}, [isOpenModal]);
 
 	const handleModalClick = (e: React.BaseSyntheticEvent | MouseEvent) => {
-		const { current } = modalRef;
 		const { target } = e;
 
-		if (current !== null && (!current.contains(target) || current === target)) {
+		// if (current !== null && (!current.contains(target) || current === target)) {
+		if (modalRef.current === target || bodyRef.current === target) {
 			if (pathname === Path.myGoals) {
 				dispatch(modalSlice.actions.close());
 				navigate(Path.myGoals);
 				document.body.style.cssText = '';
 				return;
 			}
-			if (searchParams.get('goal')) navigate(-1);
-			dispatch(modalSlice.actions.close());
-			document.body.style.cssText = '';
+			if (searchParams.get('goal')) {
+				navigate(-1);
+				dispatch(modalSlice.actions.close());
+				document.body.style.cssText = '';
+			}
 		}
+
+		// }
 	};
 
 	return (
@@ -51,7 +56,7 @@ export default function ModalSection() {
 				onClick={handleModalClick}
 				aria-hidden
 			>
-				<div className="absolute w-full h-full z-999 bg-primaryBlack-500 bg-opacity-20" />
+				<div className="absolute w-full h-full z-999 bg-primaryBlack-500 bg-opacity-20" ref={bodyRef} />
 				<div className="relative top-1/2 left-1/2" ref={modalRef}>
 					{openList.map(({ name, props }: ModalComponentState) => {
 						const config = modalList.find((ele) => ele.name === name);
